8d9b6e17d5a189f3b01210d93f11046ff57933f1
Vivek-Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-01-04 12:08:04 +0530

    added recursion

---
+++ Recursion/fibonachi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ 
+using namespace std;
+
+int fibo(int n){
+     if (n < 2) {
+         return n;
+    } else {
+        return fibo(n - 1) + fibo(n-2);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter a number to calculate fibo series : " << endl;
+    cin >> n;
+    cout << fibo(n);
+    return 0;
+}
